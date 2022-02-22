ca66800ed104c748708c4f0f519b169fa4aebcd3
Mert Duzgun <duzgunnmert@gmail.com>
2022-02-22 23:56:15 +0300

    Add gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "11cd7a12c314c88646a4"; }
+/******/ 		__webpack_require__.h = function() { return "c05deede1aad0e44954b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
