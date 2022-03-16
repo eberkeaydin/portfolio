cd9ee7c0f32487025a1f9e3ea96b55bef6662caf
eba <e.berkeaydin@gmail.com>
2022-03-17 01:21:16 +0300

    Medium external link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -27,7 +27,7 @@ const Header = () =>  (
       </li>
       <li>
         <Link href="#blog">
-          <NavLink>Blog</NavLink>
+          <NavLink to={{pathname="https://medium.com/@e.berkeaydin"}} target="_blank">Blog</NavLink>
         </Link>
       </li> 
       <li>
